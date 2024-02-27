a0a79e8b97ed96a02107894eec2874ad8649f00c
Arseny Porkhunov <arseny2606yt@gmail.com>
2024-02-28 01:10:39 +0300

    migrate to FastAPI, generate code

---
+++ solution/main.py
@@ -0,0 +1,271 @@
+from typing import List, Optional, Union, Annotated
+
+from fastapi import FastAPI, APIRouter
+from pydantic import conint
+
+from models import (
+    AuthRegisterPostRequest,
+    AuthRegisterPostResponse,
+    AuthSignInPostResponse,
+    Country,
+    CountryAlpha2,
+    CountryRegion,
+    ErrorResponse,
+    FriendsAddPostRequest,
+    FriendsAddPostResponse,
+    FriendsGetResponse,
+    FriendsRemovePostRequest,
+    FriendsRemovePostResponse,
+    MeProfilePatchRequest,
+    MeUpdatePasswordPostRequest,
+    MeUpdatePasswordPostResponse,
+    Post,
+    PostId,
+    PostsNewPostRequest,
+    UserLogin,
+    UserProfile, PingResponse,
+)
+
+app = FastAPI(
+    title='Pulse API',
+    version='1.0',
+)
+
+router = APIRouter(prefix="/api")
+
+
+@router.post(
+    '/auth/register',
+    response_model=None,
+    responses={
+        '201': {'model': AuthRegisterPostResponse},
+        '400': {'model': ErrorResponse},
+        '409': {'model': ErrorResponse},
+    },
+)
+def auth_register(
+    body: AuthRegisterPostRequest,
+) -> Union[None, AuthRegisterPostResponse, ErrorResponse]:
+    """
+    Регистрация нового пользователя
+    """
+    pass
+
+
+@router.post(
+    '/auth/sign-in',
+    response_model=AuthSignInPostResponse,
+    responses={'401': {'model': ErrorResponse}},
+)
+def auth_sign_in() -> Union[AuthSignInPostResponse, ErrorResponse]:
+    """
+    Аутентификация для получения токена
+    """
+    pass
+
+
+@router.get('/countries', response_model=List[Country])
+def list_countries(region: Optional[List[CountryRegion]] = None) -> List[Country]:
+    """
+    Получить список стран
+    """
+    pass
+
+
+@router.get(
+    '/countries/{alpha2}',
+    response_model=Country,
+    responses={'404': {'model': ErrorResponse}},
+)
+def get_country(alpha2: Annotated[str, CountryAlpha2]) -> Union[Country, ErrorResponse]:
+    """
+    Получить страну по alpha2 коду
+    """
+    pass
+
+
+@router.get(
+    '/friends',
+    response_model=List[FriendsGetResponse],
+    responses={'401': {'model': ErrorResponse}},
+)
+def friends_list(
+    limit: Optional[conint(ge=0, le=50)] = 5, offset: Optional[int] = 0
+) -> Union[List[FriendsGetResponse], ErrorResponse]:
+    """
+    Получение списка друзей
+    """
+    pass
+
+
+@router.post(
+    '/friends/add',
+    response_model=FriendsAddPostResponse,
+    responses={'401': {'model': ErrorResponse}, '404': {'model': ErrorResponse}},
+)
+def friends_add(
+    body: FriendsAddPostRequest,
+) -> Union[FriendsAddPostResponse, ErrorResponse]:
+    """
+    Добавить пользователя в друзья
+    """
+    pass
+
+
+@router.post(
+    '/friends/remove',
+    response_model=FriendsRemovePostResponse,
+    responses={'401': {'model': ErrorResponse}},
+)
+def friends_remove(
+    body: FriendsRemovePostRequest,
+) -> Union[FriendsRemovePostResponse, ErrorResponse]:
+    """
+    Удалить пользователя из друзей
+    """
+    pass
+
+
+@router.get(
+    '/me/profile',
+    response_model=UserProfile,
+    responses={'401': {'model': ErrorResponse}},
+)
+def get_my_profile() -> Union[UserProfile, ErrorResponse]:
+    """
+    Получение собственного профиля
+    """
+    pass
+
+
+@router.patch(
+    '/me/profile',
+    response_model=UserProfile,
+    responses={'401': {'model': ErrorResponse}},
+)
+def patch_my_profile(body: MeProfilePatchRequest) -> Union[UserProfile, ErrorResponse]:
+    """
+    Редактирование собственного профиля
+    """
+    pass
+
+
+@router.post(
+    '/me/updatePassword',
+    response_model=MeUpdatePasswordPostResponse,
+    responses={
+        '400': {'model': ErrorResponse},
+        '401': {'model': ErrorResponse},
+        '403': {'model': ErrorResponse},
+    },
+)
+def update_password(
+    body: MeUpdatePasswordPostRequest,
+) -> Union[MeUpdatePasswordPostResponse, ErrorResponse]:
+    """
+    Обновление пароля
+    """
+    pass
+
+
+@router.get('/ping', response_model=PingResponse)
+def ping() -> Union[PingResponse, ErrorResponse]:
+    """
+    Проверка сервера на готовность принимать запросы
+    """
+    return PingResponse(status="ok")
+
+
+@router.get(
+    '/posts/feed/my', response_model=Post, responses={'401': {'model': ErrorResponse}}
+)
+def get_my_feed(
+    limit: Optional[conint(ge=0, le=50)] = 5, offset: Optional[int] = 0
+) -> Union[Post, ErrorResponse]:
+    """
+    Получить ленту со своими постами
+    """
+    pass
+
+
+@router.get(
+    '/posts/feed/{login}',
+    response_model=Post,
+    responses={'401': {'model': ErrorResponse}, '404': {'model': ErrorResponse}},
+)
+def get_feed_by_others(
+    login: Annotated[str, UserLogin],
+    limit: Optional[conint(ge=0, le=50)] = 5,
+    offset: Optional[int] = 0,
+) -> Union[Post, ErrorResponse]:
+    """
+    Получить ленту с постами другого пользователя
+    """
+    pass
+
+
+@router.post(
+    '/posts/new', response_model=Post, responses={'401': {'model': ErrorResponse}}
+)
+def submit_post(body: PostsNewPostRequest) -> Union[Post, ErrorResponse]:
+    """
+    Отправить публикацию
+    """
+    pass
+
+
+@router.get(
+    '/posts/{post_id}',
+    response_model=Post,
+    responses={'401': {'model': ErrorResponse}, '404': {'model': ErrorResponse}},
+)
+def get_post_by_id(
+    post_id: Annotated[str, PostId]
+) -> Union[Post, ErrorResponse]:
+    """
+    Получить ленту со своими постами
+    """
+    pass
+
+
+@router.post(
+    '/posts/{post_id}/dislike',
+    response_model=Post,
+    responses={'401': {'model': ErrorResponse}, '404': {'model': ErrorResponse}},
+)
+def dislike_post(
+    post_id: Annotated[str, PostId]
+) -> Union[Post, ErrorResponse]:
+    """
+    Дизлайк поста
+    """
+    pass
+
+
+@router.post(
+    '/posts/{post_id}/like',
+    response_model=Post,
+    responses={'401': {'model': ErrorResponse}, '404': {'model': ErrorResponse}},
+)
+def like_post(
+    post_id: Annotated[str, PostId]
+) -> Union[Post, ErrorResponse]:
+    """
+    Лайк поста
+    """
+    pass
+
+
+@router.get(
+    '/profiles/{login}',
+    response_model=UserProfile,
+    responses={'401': {'model': ErrorResponse}, '403': {'model': ErrorResponse}},
+)
+def get_profile(login: Annotated[str, UserLogin]) -> Union[UserProfile, ErrorResponse]:
+    """
+    Получение профиля пользователя по логину
+    """
+    pass
+
+
+app.include_router(router)
---
+++ solution/models.py
@@ -0,0 +1,215 @@
+from enum import Enum
+from typing import List, Optional, Any
+
+from pydantic import BaseModel, Field, conint, constr, RootModel
+
+
+class PingResponse(BaseModel):
+    status: str
+
+
+class CountryAlpha2(RootModel[Any]):
+    root: constr(pattern=r'[a-zA-Z]{2}', max_length=2) = Field(
+        ...,
+        description='Двухбуквенный код, уникально идентифицирующий страну',
+        example='RU',
+    )
+
+
+class CountryRegion(Enum):
+    Europe = 'Europe'
+    Africa = 'Africa'
+    Americas = 'Americas'
+    Oceania = 'Oceania'
+    Asia = 'Asia'
+
+
+class Country(BaseModel):
+    name: constr(max_length=100) = Field(..., description='Полное название страны')
+    alpha2: CountryAlpha2
+    alpha3: constr(pattern=r'[a-zA-Z]{3}', max_length=3) = Field(
+        ..., description='Трехбуквенный код страны'
+    )
+    region: Optional[CountryRegion] = None
+
+
+class UserLogin(RootModel[Any]):
+    root: constr(pattern=r'[a-zA-Z0-9-]+', max_length=30) = Field(
+        ..., description='Логин пользователя', example='yellowMonkey'
+    )
+
+
+class UserEmail(RootModel[Any]):
+    root: constr(max_length=50) = Field(
+        ..., description='E-mail пользователя', example='yellowstone1980@you.ru'
+    )
+
+
+class UserPassword(RootModel[Any]):
+    root: constr(min_length=6, max_length=100) = Field(
+        ...,
+        description='Пароль пользователя, к которому предъявляются следующие требования:\n\n- Длина пароля не менее 6 '
+                    'символов.\n- Присутствуют латинские символы в нижнем и верхнем регистре.\n- Присутствует минимум '
+                    'одна цифра.\n',
+        example='$aba4821FWfew01#.fewA$',
+    )
+
+
+class UserIsPublic(RootModel[Any]):
+    root: bool = Field(
+        ...,
+        description='Является ли данный профиль публичным. \n\nПубличные профили доступны другим пользователям: если '
+                    'профиль публичный, любой пользователь платформы сможет получить информацию о пользователе.\n',
+        example=True,
+    )
+
+
+class UserPhone(RootModel[Any]):
+    root: constr(pattern=r'\+[\d]+') = Field(
+        ...,
+        description='Номер телефона пользователя в формате +123456789',
+        example='+74951239922',
+    )
+
+
+class UserImage(RootModel[Any]):
+    root: constr(max_length=200) = Field(
+        ...,
+        description='Ссылка на фото для аватара пользователя',
+        example='https://http.cat/images/100.jpg',
+    )
+
+
+class UserProfile(BaseModel):
+    login: UserLogin
+    email: UserEmail
+    countryCode: CountryAlpha2
+    isPublic: UserIsPublic
+    phone: Optional[UserPhone] = None
+    image: Optional[UserImage] = None
+
+
+class PostId(RootModel[Any]):
+    root: str = Field(
+        ...,
+        description='Уникальный идентификатор публикации, присвоенный сервером.',
+        example='550e8400-e29b-41d4-a716-446655440000',
+    )
+
+
+class PostContent(RootModel[Any]):
+    root: constr(max_length=1000) = Field(
+        ...,
+        description='Текст публикации.',
+        example='Свеча на 400! Покупаем, докупаем и фиксируем прибыль.',
+    )
+
+
+class PostTags(RootModel[Any]):
+    root: List[constr(max_length=20)] = Field(
+        ...,
+        description='Список тегов публикации.',
+        example=['тинькофф', 'спббиржа', 'moex'],
+    )
+
+
+class Post(BaseModel):
+    id: PostId
+    content: PostContent
+    author: UserLogin
+    tags: PostTags
+    createdAt: str = Field(
+        ...,
+        description='Серверная дата и время в момент, когда пользователь отправил данную публикацию.\nПередается в '
+                    'формате RFC3339.\n',
+        example='2006-01-02T15:04:05Z07:00',
+    )
+    likesCount: conint(ge=0) = Field(
+        ..., description='Число лайков, набранное публикацией.'
+    )
+    dislikesCount: conint(ge=0) = Field(
+        ..., description='Число дизлайков, набранное публикацией.'
+    )
+
+
+class ErrorResponse(BaseModel):
+    reason: constr(min_length=5) = Field(
+        ..., description='Описание ошибки в свободной форме'
+    )
+
+
+class AuthRegisterPostRequest(BaseModel):
+    login: UserLogin
+    email: UserEmail
+    password: UserPassword
+    countryCode: CountryAlpha2
+    isPublic: UserIsPublic
+    phone: Optional[UserPhone] = None
+    image: Optional[UserImage] = None
+
+
+class AuthRegisterPostResponse(BaseModel):
+    profile: UserProfile
+
+
+class AuthSignInPostResponse(BaseModel):
+    token: constr(min_length=20) = Field(
+        ...,
+        description='Сгенерированный токен пользователя',
+        example='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+                '.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ'
+                '.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    )
+
+
+class MeProfilePatchRequest(BaseModel):
+    countryCode: Optional[CountryAlpha2] = None
+    isPublic: Optional[UserIsPublic] = None
+    phone: Optional[UserPhone] = None
+    image: Optional[UserImage] = None
+
+
+class MeUpdatePasswordPostRequest(BaseModel):
+    oldPassword: UserPassword
+    newPassword: UserPassword
+
+
+class MeUpdatePasswordPostResponse(BaseModel):
+    status: str = Field(
+        ..., description='Должно принимать значение `ok`.', example='ok'
+    )
+
+
+class FriendsAddPostRequest(BaseModel):
+    login: UserLogin
+
+
+class FriendsAddPostResponse(BaseModel):
+    status: str = Field(
+        ..., description='Должно принимать значение `ok`.', example='ok'
+    )
+
+
+class FriendsRemovePostRequest(BaseModel):
+    login: UserLogin
+
+
+class FriendsRemovePostResponse(BaseModel):
+    status: str = Field(
+        ..., description='Должно принимать значение `ok`.', example='ok'
+    )
+
+
+class FriendsGetResponse(BaseModel):
+    login: UserLogin
+    addedAt: str = Field(
+        ...,
+        description='Время и дата, когда данный пользователь был добавлен в друзья в последний раз.\n\nПередается в '
+                    'формате RFC3339.\n',
+        example='2006-01-02T15:04:05Z07:00',
+    )
+
+
+class PostsNewPostRequest(BaseModel):
+    content: PostContent
+    tags: PostTags
